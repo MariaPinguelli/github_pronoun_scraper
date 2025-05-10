2feb22ec2bedba422d6fc842134cef00d076ea8e
MariaPinguelli <mariaferpinguelli@hotmail.com>
2025-05-10 14:39:29 -0300

    inferência de pronomes e atualização das estruturas de dados

--- main.py
+++ main.py
@@ -8,13 +8,24 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from dotenv import load_dotenv
+from WikiGendersort.Wiki_Gendersort import wiki_gendersort
 
 print("---------- Iniciando Script ----------")
 
 load_dotenv()
 
+pronoun_dict = {
+    'M': 'he/him',
+    'F': 'she/her',
+    'UNK': 'unknown',
+    'INI': 'unknown',
+    'UNI': 'unknown',
+}
+
+WG = wiki_gendersort()
+
 chrome_options = Options()
-chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--headless')
 driver = webdriver.Chrome(options=chrome_options)
 
 wait = WebDriverWait(driver, 3)
@@ -47,6 +58,7 @@ i = 0
 
 while True:
     i += 1
+    print('Procurando colaboradores do repositório... | ', i)
     response = requests.get(f"{github_api_url}repos/JabRef/jabref/contributors?page={i}&per_page=100", headers=headers)
     data = response.json()
     if not data:
@@ -55,14 +67,16 @@ while True:
     for contributor in data:
         if not any(user['login'] == contributor['login'] for user in saved_users) and '[bot]' not in contributor['login']:
             contributors.append(contributor)
+    
+    print("--------------------------------------\n\n")
 
 if contributors:
     saved_users.extend(contributors)
     with open(users_file, 'w') as f:
         json.dump(saved_users, f, indent=2)
 
-    print("before driver.get")
     # Login
+    print('Fazendo login...')
     driver.get(f"{github_url}login")
 
     username_input = driver.find_element(By.ID, 'login_field')
@@ -70,42 +84,69 @@ if contributors:
 
     username_input.send_keys(os.getenv('GITHUB_USERNAME'))
     password_input.send_keys(os.getenv('GITHUB_PASSWORD'))
-    print('input data')
 
     login_button = driver.find_element(By.NAME, 'commit')
     login_button.click()
-    print('clicked')
 
-    time.sleep(30)
+    time.sleep(15)
 
     for contributor in contributors:
         file_path = f"./users/{contributor['login']}.json"
+        user = dict()
         
-        if not os.path.exists(file_path):
-            # Criando arquivo vazio
+        if not os.path.exists(file_path): # carregar se já tiver
+            print('Criando arquivo: ', file_path)
             os.makedirs(os.path.dirname(file_path), exist_ok=True)
             with open(file_path, 'w') as f:
                 pass
-
+        # else:
+        print('Lendo arquivo: ', file_path)
+        with open(file_path, 'r') as f:
+            file_content = f.read()
+            try:
+                user = json.loads(file_content)
+            except:
+                pass
+              
+        if not user or len(file_content) == 0:
+            print(f"Buscando usuário {contributor['login']} no github")
             response = requests.get(f"{github_api_url}users/{contributor['login']}", headers=headers)
             user = response.json()
 
-            driver.get(f"{github_url}{contributor['login']}")
-            # print("LINHA 94")
+        if 'declared_pronoun' not in user:
+            print(f"Buscando declared_pronoun do usuário {user['login']} no github")
+            driver.get(f"{github_url}{user['login']}")
 
             try:
                 pronouns = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '[itemprop="pronouns"]'))).text
-                # print(pronouns)
-                user['pronouns'] = pronouns
-                print(pronouns)
+                user['declared_pronoun'] = pronouns
             except Exception as e:
-                print(f"\n\nERRO {e}\n\n")
-                user['pronouns'] = 'no_pronouns'
+                print(f"\n\nPronome não declarado\n\n")
+                user['declared_pronoun'] = 'unknown'
 
-            with open(file_path, 'w') as f:
-                json.dump(user, f, indent=2)
+        if 'infered_pronoun' not in user:
+            print(f"Inferindo infered_pronoun do usuário {user['login']}")
+            if user['name']:
+                username = user['name'].split()[0] 
+                user['infered_pronoun'] = pronoun_dict[WG.assign(username)]
+            else:
+                user['infered_pronoun'] = 'unknown'
+
+        if user['declared_pronoun'] != 'unknown':
+            user['pronoun'] = user['declared_pronoun']
+        elif user['infered_pronoun'] != 'unknown':
+            user['pronoun'] = user['infered_pronoun']
+        else:
+            user['pronoun'] = 'unknown'
+
+        with open(file_path, 'w') as f:
+            json.dump(user, f, indent=2)
+        
+        print("--------------------------------------\n\n")
 
 else:
     print("Não existem novos contribuidores a serem adicionados ao banco de dados.")
+
+print("---------- Fim do Script ----------")
 
 driver.quit()
