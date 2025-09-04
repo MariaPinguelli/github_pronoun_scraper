80c0cfc7b9f8b1d6bd9656c7c233107154aff9fe
MariaPinguelli <mariaferpinguelli@hotmail.com>
2025-09-04 18:12:16 -0300

    novo data analysis

--- utils.py
+++ utils.py
@@ -1,28 +1,44 @@
 from WikiGendersort.Wiki_Gendersort import wiki_gendersort
 
+def get_pronoun(user):
+    invalid_values = [None, '', 'unknown']
+    
+    declared = user.get('declared_pronouns')
+    if declared not in invalid_values:
+        return declared
+    
+    infered = user.get('infered_pronouns')
+    if infered not in invalid_values:
+        return infered
+    
+    return 'unknown'
+
 def process_data(raw_data):
     gendersort = wiki_gendersort()
 
     pronoun_dict = {
         'M': 'he/him',
         'F': 'she/her',
-        'UNK': '',
-        'INI': '',
-        'UNI': ''
+        'UNK': 'unknown',
+        'INI': 'unknown',
+        'UNI': 'unknown'
     }
+    
+    raw_pronouns = raw_data.get('pronouns')
+    declared_pronouns = 'unknown' if raw_pronouns in [None, ''] else raw_pronouns
     
     processed_data = {
-        'name': None,
-        'login': None,
-        'declared_pronouns': None,
-        'infered_pronouns': None,
+        'name': raw_data.get('name'),
+        'login': raw_data.get('login', 'unknown'),
+        'declared_pronouns': declared_pronouns,
+        'infered_pronouns': 'unknown',
+        'pronouns': 'unknown'
     }
-
-    processed_data['name'] = raw_data['name']
-    processed_data['login'] = raw_data['login']
-    processed_data['declared_pronouns'] = raw_data['pronouns']
     
-    if (raw_data['name']):
-        processed_data['infered_pronouns'] = pronoun_dict[gendersort.assign(raw_data['name'])]
+    if processed_data['name']:
+        gender = gendersort.assign(processed_data['name'])
+        processed_data['infered_pronouns'] = pronoun_dict.get(gender, 'unknown')
+    
+    processed_data['pronouns'] = get_pronoun(processed_data)
     
     return processed_data
